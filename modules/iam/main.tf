64b08a9cd6ee6db3634323b5cdd705a925adbd2b
abhishekchauhan98 <abhishek.chauhan1@tothenew.com>
2020-04-16 10:33:37 +0530

    IAM second fix

--- modules/iam/main.tf
+++ modules/iam/main.tf
@@ -51,27 +51,27 @@ resource "aws_iam_role" "node" {
 EOF
 }
 
-resource "aws_iam_role_policy_attachement" "AmazonEKSClusterPolicy" {
+resource "aws_iam_role_policy_attachment" "AmazonEKSClusterPolicy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
   role = aws_iam_role.node.name
 }
 
-resource "aws_iam_role_policy_attachement" "AmazonEKSWorkerNodePolicy" {
+resource "aws_iam_role_policy_attachment" "AmazonEKSWorkerNodePolicy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
   role = aws_iam_role.node.name
 }
 
-resource "aws_iam_role_policy_attachement" "AmazonEC2ContainerRegistryReadOnly" {
+resource "aws_iam_role_policy_attachment" "AmazonEC2ContainerRegistryReadOnly" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
   role = aws_iam_role.node.name
 }
 
-resource "aws_iam_role_policy_attachement" "AmazonEKSServicePolicy" {
+resource "aws_iam_role_policy_attachment" "AmazonEKSServicePolicy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSServicePolicy"
   role = aws_iam_role.node.name
 }
 
-resource "aws_iam_role_policy_attachement" "AmazonEKS_CNI_Policy" {
+resource "aws_iam_role_policy_attachment" "AmazonEKS_CNI_Policy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
   role = aws_iam_role.node.name
 }
