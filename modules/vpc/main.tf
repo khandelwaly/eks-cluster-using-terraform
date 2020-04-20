114070650db1caf911e545010f89aab5aa65963e
yash-khandelwal-ttn <yash.khandelwal@tothenew.com>
2020-04-20 14:39:17 +0530

    ADD NAT Gateway

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -39,6 +39,8 @@ resource "aws_subnet" "public" {
   }
 }
 
+
+
 resource "aws_internet_gateway" "eks_vpc_igw" {
  vpc_id = aws_vpc.eks_vpc.id
  tags = {
@@ -73,4 +75,19 @@ resource "aws_route_table_association" "eks_subnet_association" {
   route_table_id = aws_route_table.eks_vpc_rt_public.id
 } 
 
+## NAT GATEWAY ###
+resource "aws_eip" "eks_eip" {
+  vpc      = true
+}
 
+resource "aws_nat_gateway" "eks_vpc_nw" {
+  allocation_id = aws_eip.eks_eip.id
+  subnet_id     = aws_subnet.public[0].id
+}
+
+resource "aws_route" "eks_vpc_private_nat_route" {
+  route_table_id         = aws_vpc.eks_vpc.default_route_table_id
+  destination_cidr_block = "0.0.0.0/0"
+  nat_gateway_id          = aws_nat_gateway.eks_vpc_nw.id
+  # depends_on             = [aws_vpc.eks_vpc.default_route_table_id]
+}
